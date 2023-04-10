6e2e993a313d9e58798504ffa9a72541664f778d
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-10 22:29:04 +0200

    feat: add article details design

--- src/components/sections/ArticleDetails.jsx
+++ src/components/sections/ArticleDetails.jsx
@@ -1,5 +1,37 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 const ArticleDetails = () => {
-  return <div>ArticleDetails </div>;
+  const { selectedArticle } = useSelector((state) => state.articles);
+  return (
+    <div className="py-10 space-y-6">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <img
+          src={selectedArticle.urlToImage ?? ""}
+          className="object-cover object-center rounded-xl"
+        />
+        <div className="space-y-3 sm:order-first">
+          <p className="text-sm text-slate-500">
+            {selectedArticle?.publishedAt?.substring(0, 10)}
+          </p>
+          <h3 className="font-bold text-slate-700">{selectedArticle.title}</h3>
+          <div>
+            <p className="text-sm text-slate-500">Publisher</p>
+            <p className="text-sm text-cyan-700">{selectedArticle.author}</p>
+          </div>
+        </div>
+      </div>
+      <div className="space-y-4 md:w-4/5">
+        <p className="text-slate-700">{selectedArticle.description ?? ""}</p>
+        <p className="text-slate-700">
+          {selectedArticle.content ?? selectedArticle.description ?? ""}
+        </p>
+        <Link to={selectedArticle.url} className="text-base text-cyan-600">
+          read more
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleDetails;
