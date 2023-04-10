f9ed56e4e2b5caea19074a01db05cdc112266ee2
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-10 23:14:28 +0200

    feat: add navigate back functionality to article details component

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import Categories from "../sections/Categories";
 import Sources from "../sections/Sources";
 import Footer from "./Footer";
 import Header from "./Header";
 
 const PageLayout = () => {
+  const { articleId } = useParams();
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 max-w-6xl px-5 pb-10 space-y-10 lg:mx-auto">
-        <Categories />
+        {!articleId && <Categories />}
+
         <div className="grid grid-cols-1 gap-12 md:grid-cols-4">
           <div className="md:col-span-3">
             <Outlet />
--- src/components/sections/ArticleDetails.jsx
+++ src/components/sections/ArticleDetails.jsx
@@ -1,34 +1,46 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ArticleDetails = () => {
   const { selectedArticle } = useSelector((state) => state.articles);
+  const navigate = useNavigate();
+  const handleNavigateBack = () => navigate(-1);
   return (
-    <div className="py-10 space-y-6">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <img
-          src={selectedArticle.urlToImage ?? ""}
-          className="object-cover object-center rounded-xl"
-        />
-        <div className="space-y-3 sm:order-first">
-          <p className="text-sm text-slate-500">
-            {selectedArticle?.publishedAt?.substring(0, 10)}
-          </p>
-          <h3 className="font-bold text-slate-700">{selectedArticle.title}</h3>
-          <div>
-            <p className="text-sm text-slate-500">Publisher</p>
-            <p className="text-sm text-cyan-700">{selectedArticle.author}</p>
+    <div className="py-10 space-y-10">
+      <button
+        onClick={handleNavigateBack}
+        className="text-base text-cyan-700 hover:underline"
+      >
+        back
+      </button>
+      <div className="space-y-6 ">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <img
+            src={selectedArticle.urlToImage ?? ""}
+            className="object-cover object-center rounded-xl"
+          />
+          <div className="space-y-3 sm:order-first">
+            <p className="text-sm text-slate-500">
+              {selectedArticle?.publishedAt?.substring(0, 10)}
+            </p>
+            <h3 className="font-bold text-slate-700">
+              {selectedArticle.title}
+            </h3>
+            <div>
+              <p className="text-sm text-slate-500">Publisher</p>
+              <p className="text-sm text-cyan-700">{selectedArticle.author}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="space-y-4 md:w-4/5">
-        <p className="text-slate-700">{selectedArticle.description ?? ""}</p>
-        <p className="text-slate-700">
-          {selectedArticle.content ?? selectedArticle.description ?? ""}
-        </p>
-        <Link to={selectedArticle.url} className="text-base text-cyan-600">
-          read more
-        </Link>
+        <div className="space-y-4 md:w-4/5">
+          <p className="text-slate-700">{selectedArticle.description ?? ""}</p>
+          <p className="text-slate-700">
+            {selectedArticle.content ?? selectedArticle.description ?? ""}
+          </p>
+          <Link to={selectedArticle.url} className="text-base text-cyan-600">
+            read more
+          </Link>
+        </div>
       </div>
     </div>
   );
