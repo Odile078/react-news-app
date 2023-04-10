7383504c22891708a349d053b95fc1f4a9cfade6
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-10 15:40:00 +0200

    feat: add responsiveness to search form

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,16 +4,16 @@ const Header = () => {
     e.preventDefault();
   };
   return (
-    <header className="flex items-center justify-between w-full max-w-6xl px-5 py-4 bg-slate-100 lg:mx-auto">
+    <header className="flex flex-wrap items-center justify-between w-full max-w-6xl gap-4 px-5 py-4 bg-slate-100 lg:mx-auto">
       <h1 className="text-5xl logo-font">News</h1>
       <form
-        className="flex p-1 border rounded-xl border-slate-100"
+        className="flex p-1 px-2 bg-white border rounded-lh border-slate-100"
         onSubmit={handleSubmitForm}
       >
         <input
           type="text"
           placeholder="search article"
-          className="px-5 py-2 border-0 outline-none focus:outline-none text-slate-600 placeholder:text-slate-600"
+          className="px-1 py-2 border-0 outline-none focus:outline-none text-slate-600 placeholder:text-slate-600"
         />
         <button type="button">
           <IoSearchOutline className="text-slate-500" />
