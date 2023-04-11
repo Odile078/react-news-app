7fbf40408f8b81c57594bbae344bfeff4df7588e
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-11 18:44:16 +0200

    feat: modify the logo

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import { TbSpeakerphone } from "react-icons/tb";
+
 const Footer = () => {
   return (
-    <footer className="w-full max-w-6xl px-5 py-10 bg-slate-500 lg:mx-auto">
-      <h1 className="text-5xl logo-font text-slate-50">News</h1>
+    <footer className="w-full max-w-6xl px-5 py-10 bg-slate-200 lg:mx-auto">
+      <Link to="/" className="flex items-end gap-2">
+        <h1 className="text-3xl logo-font">News</h1>
+        <TbSpeakerphone className="text-4xl" />
+      </Link>
     </footer>
   );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,16 @@
 import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { TbSpeakerphone } from "react-icons/tb";
+
 const Header = () => {
   const handleSubmitForm = (e) => {
     e.preventDefault();
   };
   return (
-    <header className="flex flex-wrap items-center justify-between w-full max-w-6xl gap-4 px-5 py-4 bg-slate-100 lg:mx-auto">
-      <Link to="/">
-        <h1 className="text-5xl logo-font">News</h1>
+    <header className="flex flex-wrap items-center justify-between w-full max-w-6xl gap-4 px-5 py-4 bg-slate-200 lg:mx-auto">
+      <Link to="/" className="flex items-end gap-2">
+        <h1 className="text-3xl logo-font">News</h1>
+        <TbSpeakerphone className="text-4xl" />
       </Link>
       <form
         className="flex p-1 px-2 bg-white border rounded-lh border-slate-100"
