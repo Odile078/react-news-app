b97dbc7082f4457b07591800ecb88194f1979a5f
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-17 12:07:36 +0200

    feat: add api key in the env file

--- src/features/articlesSlice.js
+++ src/features/articlesSlice.js
@@ -1,15 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import API_KEY from "../data/api";
+// import API_KEY from "../data/api";
 import { nanoid } from "nanoid";
-import {
-  endOfWeek,
-  format,
-  lastDayOfWeek,
-  startOfDay,
-  startOfWeek,
-} from "date-fns";
+import { format, startOfWeek } from "date-fns";
 
+const API_KEY = import.meta.env.VITE_API_KEY;
 const initialState = {
   isLoading: false,
   articles: [],
