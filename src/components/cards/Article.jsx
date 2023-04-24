517f7d0530e37ecfcf8b6d76fb652873706dde67
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-24 14:47:40 +0200

    feat: make article card clickable

--- src/components/cards/Article.jsx
+++ src/components/cards/Article.jsx
@@ -22,6 +22,7 @@ const Article = ({ article, firstArticle = false, smallSize = false }) => {
       className={`flex w-full bg-slate-50 border border-slate-50 hover:bg-slate-100 hover:shadow-md cursor-pointer gap-2 justify-between ${
         firstArticle ? "lg:row-span-2 lg:col-span-2 " : ""
       } ${smallSize ? "flex-col" : "flex-col"}`}
+      onClick={() => handleSelectArticle(article)}
     >
       <img
         src={
@@ -39,7 +40,7 @@ const Article = ({ article, firstArticle = false, smallSize = false }) => {
       />
       <div
         className={`p-4 space-y-4 justify-self-end ${
-          firstArticle ? "my-10" : ""
+          firstArticle ? "my-10-" : ""
         }`}
       >
         <div>
@@ -48,16 +49,16 @@ const Article = ({ article, firstArticle = false, smallSize = false }) => {
               smallSize ? "text-sm" : "text-base"
             } font-bold text-slate-400`}
           >
-            {article.title}
+            {article.title ? ` ${article.title?.substring(0, 60)}...` : ""}
           </h5>
           <p className="pb-2 text-sm text-slate-400">
             {article?.publishedAt?.substring(0, 10)}
           </p>
         </div>
 
-        <p className={`text-slate-400 ${smallSize ? "hidden" : ""}`}>
+        {/* <p className={`text-slate-400 ${smallSize ? "hidden" : ""}`}>
           {article.description}
-        </p>
+        </p> */}
         <button
           onClick={() => handleSelectArticle(article)}
           className="text-sm font-bold text-cyan-700"
